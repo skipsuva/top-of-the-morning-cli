bfcdde5d701283e580bbafeccb162321c2593f22
Aemrox <adam.may2164@gmail.com>
2015-12-14 17:22:05 -0500

    wrote the IO for adding new sources

--- app/runners/top_of_the_morning_io.rb
+++ app/runners/top_of_the_morning_io.rb
@@ -25,12 +25,17 @@ class ImportIO
 
   def selection
     x = list_stories
-    puts "\n Please enter the number of the story you'd like to read - or 'exit' to quit:"
+    puts "\n Please enter the number of the story you'd like to read
+     - type 'add' to add more sources
+     - or 'exit' to quit:"
     loop do
       selection = gets.chomp
       if selection.downcase == "exit"
         exit_program
         break
+      elsif selection.downcase == "add"
+        add_sources
+        x = list_stories
       elsif (1..(x.length)).include?(selection.to_i)
         system "open #{x[selection.to_i-1].url}"
       else
@@ -38,6 +43,82 @@ class ImportIO
       end
     end
 
+  end
+
+  def add_sources
+    puts "Which source would you like to add from?
+    -Reddit
+    -Product Hunt
+    -Stack Overflow"
+    loop do
+      selection = gets.chomp
+      if selection.downcase == "reddit"
+        break if add_to_reddit
+        puts "make another selection, or type 'exit'"
+      elsif selection.downcase == "product hunt"
+        break if add_to_product_hunt
+        puts "make another selection, or type 'exit'"
+      elsif selection.downcase == "stack overflow"
+        break if add_to_stack_overflow
+        puts "make another selection, or type 'exit'"
+      elsif seleciton.downcase == 'exit'
+        break
+      else
+        puts "sorry, I didn't get, please make a valid selection"
+      end
+    end
+  end
+
+  def add_to_reddit
+    loop do
+      puts "Type in any subreddit, or type 'exit':"
+      selection = (gets.chomp).downcase
+      if selection == "exit"
+        return false
+      elsif RedditValidator.new(selection).validate
+        importer.source_add("Reddit",selection)
+        puts "you added the #{selection} subreddit"
+        return true
+      else
+        puts "I'm sorry, that's not a valid subreddit, try again!"
+      end
+    end
+  end
+
+  def add_to_product_hunt
+    choices = ["games","books","podcasts"]
+    loop do
+      puts "Select one of the following categories, or type 'exit':
+      -'Games'
+      -'Books'
+      -'Podcasts'"
+      selection = (gets.chomp).downcase
+      if selection == "exit"
+        return false
+      elsif choices.include?(selection)
+        importer.source_add("Product Hunt",selection)
+        puts "you added the #{selection} tag"
+        return true
+      else
+        puts "I'm sorry, that's not a valid tag, try again!"
+      end
+    end
+  end
+
+  def add_to_stack_overflow
+    loop do
+      puts "Type in any Stack Overflow tag, or type 'exit':"
+      selection = (gets.chomp).downcase
+      if selection == "exit"
+        return false
+      elsif StackOverflowValidator.new(selection).validate
+        importer.source_add("Stack Overflow",selection)
+        puts "you added the #{selection} tag"
+        return true
+      else
+        puts "I'm sorry, that's not a valid tag, try again!"
+      end
+    end
   end
 
   def exit_program
