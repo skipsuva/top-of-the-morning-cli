2f665c98a5ce7f3659920dd06755c9c6007d8367
Aemrox <adam.may2164@gmail.com>
2015-12-14 14:27:43 -0500

    stack overflow importer and class done

---
+++ app/data_fetchers/stack_overflow_importer.rb
@@ -0,0 +1,20 @@
+class StackOverflowImporter
+  attr_accessor :tag
+
+  def initialize(tag='ruby')
+    @tag = tag
+  end
+
+  def import
+    RubyStackoverflow.featured_questions({page: 1, tagged:tag}).data
+  end
+
+  def grab_top
+    raw = self.import
+    raw.sort_by!{|story| story.bounty_amount}
+    title = raw[-1].title
+    bounty = raw[-1].bounty_amount
+    url = raw[-1].link
+    StackOverflowStory.new(title,url,bounty)
+  end
+end
